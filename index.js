6188ece509b61f27bcb009fff4e7daecbc225b0d
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-18 19:02:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musicians,instruments){
+  beatlesString=[]
   for (let i=0; i>0;i++){
-  return `${musicians[i]} plays ${instruments[i]}`
+  return beatlesString.push( `${musicians[i]} plays ${instruments[i]}`)
   }}
   
