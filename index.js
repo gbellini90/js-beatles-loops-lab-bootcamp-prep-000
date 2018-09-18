48196bcb544672930edaecec66e1bd1fac06c212
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-18 19:28:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ function johnLennonFacts(facts){
     return jLFacts
 }
 
+/* or with a while loop
+function johnLennonFacts(facts){
+var newJohnFacts=[];
+var i =0;
+while(i<facts.length){
+newJohnFacts.push(`${facts[i]}!!!`)
+i++}
+return newJohnFacts.join()} */
+
 function iLoveTheBealtes(number){
-  
+  var array=[];
+  var i=0
+  do{array.push('I love the Beatles!')
+  number++}
+  while(number<15)
+  return array
 }
