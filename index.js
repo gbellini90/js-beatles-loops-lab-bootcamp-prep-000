6b3af99262ab9b93496934a9880c96591892ea23
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-18 19:02:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians,instruments){
   for (let i=0; i>0;i++){
-  return `${musicians[i]} plays ${instruments[i]} }
-  }
-}
+  return `${musicians[i]} plays ${instruments[i]}`
+  }}
+  
